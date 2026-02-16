31bb5cc574edd94f4a2e2a7f18defad513d95acb
Lyes Nefir <nefirlyes@gmail.com>
2026-02-16 16:23:20 +0100

    [fetch_stage] Updating the old function fetch_update and divide it into multiple functions so it becomes more readable, documentation on the way

--- core/src/fetch/ifetch.h
+++ core/src/fetch/ifetch.h
@@ -13,43 +13,45 @@ SC_MODULE(fetch) {
 
 	sc_out<sc_bv<32>> imem_req_addr;
   sc_out<bool>      imem_req_valid;
-    
   sc_in<sc_bv<32>>  imem_resp_instr;
-	sc_out<bool> 	  imem_resp_ready;
-	sc_in<bool>	  imem_resp_error;
+	sc_out<bool> 	    imem_resp_ready;
    
 //=================================================================================
 //     Interface with Decoding stage      
 //=================================================================================
-    	sc_out<sc_bv<32>>	out_if2id_pc_curr; 
-	sc_out<bool>		out_if2id_valid;
-	sc_in<bool>		out_if2id_ready;
+  sc_out<sc_bv<32>>	out_if2id_pc_curr; 
+	sc_out<bool>		  out_if2id_valid;
+	sc_in<bool>		    out_if2id_ready;
 	sc_out<sc_bv<32>>	out_if2id_instr;
-    	sc_out<sc_bv<32>>  	out_if2id_pc_next;
+  sc_out<sc_bv<32>> out_if2id_pc_next;
 
 
-	sc_in<bool>		hazard_unit_flush_d; 
-	sc_in<bool>		pc_branching;
+	sc_in<bool>		    flush_d; 
+	sc_in<bool>		    pc_src;
 	sc_in<sc_bv<32>> 	pc_branching_target;
 
-	sc_in<bool>		hazard_unit_stall_f;
-	sc_in<bool>		hazard_unit_stall_d;
+	sc_in<bool>		    stall_f;
+	sc_in<bool>		    stall_d;
 
 	void fetch_update();
+  void fifo_concat();
+  void fifo_unconcat();
+  void fifo_handling();
+  void imem_request();
 	void trace(sc_trace_file *tf);
 
 	fifo<IF2ID_SIZE,1> fifo_element;
 
-	sc_signal<bool>			 in_if2id_ready;
-	sc_signal<bool>			 in_if2id_valid;
+	sc_signal<bool>			          in_if2id_ready;
+	sc_signal<bool>			          in_if2id_valid;
 	
-	sc_signal<bool>               	 if2id_empty;
-	sc_signal<bool>              	 if2id_full;
+	sc_signal<bool>               if2id_empty;
+	sc_signal<bool>              	if2id_full;
 
-	sc_signal<sc_bv<IF2ID_SIZE>>	 if2id_in;
-	sc_signal<sc_bv<IF2ID_SIZE>>	 if2id_out; 
+	sc_signal<sc_bv<IF2ID_SIZE>>	if2id_in;
+	sc_signal<sc_bv<IF2ID_SIZE>>	if2id_out; 
 
-	sc_signal<sc_bv<32>>		 pc_reg;
+	sc_signal<sc_bv<32>>		      pc_reg;
 
 	
 	SC_CTOR(fetch) : fifo_element("if2id"){
@@ -69,8 +71,19 @@ SC_MODULE(fetch) {
 		fifo_element.empty_o	(if2id_empty);
 
 		SC_METHOD(fetch_update);
-		sensitive << clk.pos() << arstn << imem_resp_instr << imem_resp_error << out_if2id_ready  << hazard_unit_stall_f << hazard_unit_stall_d << hazard_unit_flush_d << pc_branching ; 
+		sensitive << clk.pos() << arstn << imem_resp_instr << out_if2id_ready  << stall_f << stall_d << flush_d << pc_src ;
 		
+		SC_METHOD(fifo_concat);
+		sensitive << imem_resp_instr;
+		
+		SC_METHOD(fifo_unconcat);
+		sensitive << if2id_out;
+		
+		SC_METHOD(fifo_handling);
+		sensitive << if2id_full << stall_d << flush_d;
+		
+		SC_METHOD(imem_request);
+		sensitive << in_if2id_ready << stall_f;
     }
 };
 
